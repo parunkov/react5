1760ad65b1f8bf6bc96f42a0b19954e910197a07
parunkov <parunkov@gmail.com>
2023-06-26 15:29:17 +0500

    product data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Product } from './components/Prouct';
+import { products } from './data/products';
+console.log(products);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='container mx-auto max-w-2xl pt-5'>
+      <Product />
     </div>
-  );
+  )
 }
 
 export default App;
+
+  // const [count, setCount] = useState(0);
+
+  // return e('div', {className: 'container'}, [
+  //   e('h1', {className: 'font-bold', key: 1}, `${count}`),
+  //   e('button', {
+  //     className: 'py-2 px-4 border', 
+  //     key: 2,
+  //     onClick: () => setCount(count + 1),      
+  //   }, 'Click Me!')
+  // ]);
---
+++ src/components/Prouct.tsx
@@ -0,0 +1,5 @@
+export function Product() {
+    return(
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">Product</div>
+    )
+}
