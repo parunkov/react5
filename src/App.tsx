ed4cb2033d4f99324557aecb1de35a877e1f110a
parunkov <parunkov@gmail.com>
2023-06-27 09:35:20 +0500

    hook created

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from 'react';
 import { Product } from './components/Prouct';
-// import { products } from './data/products';
-import axios, {AxiosError} from 'axios';
-import { IProduct } from './models';
+import { useProducts } from './hooks/products';
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  async function fetchProucts() {
-    try {
-      setError('');
-      setLoading(true);
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
-      setProducts(response.data);
-      setLoading(false);
-    } catch (e: unknown) {
-      const error = e as AxiosError;
-      setLoading(false);
-      setError(error.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchProucts();
-  }, []);
+  const { products, error, loading } = useProducts();
 
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
