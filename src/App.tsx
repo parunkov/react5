416ec62decb572699ea78b699854c6a4bbbc912c
parunkov <parunkov@gmail.com>
2023-06-27 09:33:46 +0500

    hook creating

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Product } from './components/Prouct';
-import { products } from './data/products';
+// import { products } from './data/products';
+import axios, {AxiosError} from 'axios';
+import { IProduct } from './models';
 
 function App() {
-  return(
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function fetchProucts() {
+    try {
+      setError('');
+      setLoading(true);
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProucts();
+  }, []);
+
+  return (
     <div className='container mx-auto max-w-2xl pt-5'>
+      {loading && <p className='text-center'>Loading...</p>}
+      {error && <p className='text-center text-red-600'>{error}</p>}
       {products.map(product => <Product product={product} key={product.id} />)}
     </div>
   )
@@ -17,8 +43,8 @@ export default App;
   // return e('div', {className: 'container'}, [
   //   e('h1', {className: 'font-bold', key: 1}, `${count}`),
   //   e('button', {
-  //     className: 'py-2 px-4 border', 
+  //     className: 'py-2 px-4 border',
   //     key: 2,
-  //     onClick: () => setCount(count + 1),      
+  //     onClick: () => setCount(count + 1),
   //   }, 'Click Me!')
   // ]);
