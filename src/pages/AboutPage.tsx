20361824f0f601bd87dc26537135446c9aa3c5e9
parunkov <parunkov@gmail.com>
2023-06-28 08:53:44 +0500

    nav

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage() {
+    return(
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut distinctio ipsum quos nam blanditiis repellendus esse a odit quae in adipisci provident reiciendis, eligendi rem natus? Quo sunt dicta ex voluptate laudantium. Aspernatur deserunt eligendi reprehenderit hic eaque est commodi quidem aliquam excepturi, quam nemo fugiat dolorum accusantium nisi sint.</div>
+    )
+};
---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { IProduct } from "../models";
+import { Loader } from "../components/Loader";
+import { ErrorMessage } from "../components/ErrorMessage";
+import { Product } from "../components/Prouct";
+import { Modal } from "../components/Modal";
+import { CreateProduct } from "../components/CreateProduct";
+import { useProducts } from "../hooks/products";
+import { ModalContext } from "../context/ModalContext";
+
+export function ProductPage() {
+    const { products, error, loading, addProduct } = useProducts();
+    const { modal, open, close } = useContext(ModalContext);
+
+    const createHandler = (product: IProduct) => {
+        close();
+        addProduct(product);
+    };
+
+    return (
+        <div className='container mx-auto max-w-2xl pt-5'>
+            {loading && <Loader />}
+            {error && <ErrorMessage error={error} />}
+            {products.map(product => <Product product={product} key={product.id} />)}
+
+            {modal && <Modal title='Create new product' onClose={close}>
+                <CreateProduct onCreate={createHandler} />
+            </Modal>}
+            <button
+                className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+                onClick={open}
+            >+</button>
+        </div>
+    );
+};
